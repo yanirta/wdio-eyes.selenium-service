71bb8b3dc8a180a0f253ae4cd3b684835dde9281
Yanir Taflev <yanir@Yanirs-MacBook-Pro-2.local>
2018-07-10 10:37:55 +0300

    camel casing, fixing serverurl bug, server up

--- test/specs/simple_web.js
+++ test/specs/simple_web.js
@@ -2,21 +2,21 @@ describe('Simple cases', function () {
     this.timeout(300000);
     describe("Test native App", function () {
         it("test-function-1",function () {
-            browser.EyesOpen("Hello world 1");
+            browser.eyesOpen("Hello world 1");
             browser.url('/helloworld');
-            browser.EyesCheckWindow("Homepage");
+            browser.eyesCheckWindow("Homepage");
             browser.click('button');
-            browser.EyesCheckWindow("After click");
-            browser.EyesClose(false);
+            browser.eyesCheckWindow("After click");
+            browser.eyesClose(false);
         });
 
         it("test-function-2",function () {
-            browser.EyesOpen("Hello world 2");
+            browser.eyesOpen("Hello world 2");
             browser.url('/helloworld');
-            browser.EyesCheckWindow("Homepage");
+            browser.eyesCheckWindow("Homepage");
             browser.click('button');
-            browser.EyesCheckWindow("After click");
-            browser.EyesClose(false);
+            browser.eyesCheckWindow("After click");
+            browser.eyesClose(false);
         });
     });
 });
